cfe4b0eacba09920cf1d6fcd17ba8734bbfc43b2
Suresh Sarda <sureshssarda@gmail.com>
2017-10-17 09:21:18 +0530

    Extended Euclid algorithm

--- clrs/31_number_theoritic_algorithms/euclid_gcd.py
+++ clrs/31_number_theoritic_algorithms/euclid_gcd.py
@@ -36,12 +36,34 @@ def gcd(a, b):
         return gcd(b, a % b)
 
 
+def extended_euclid(a, b):
+    """
+    Extended Euclid Algorithm. Given a and b, return d, x, y s.t. they satisfy the equation: d = gcd(a, b) = ax + by
+
+    :param a: first number
+    :type a: int
+    :param b: second number
+    :type b: int
+    :return: tuple(d, x, y) values in the equation d = gcd(a, b) = ax + by
+    :rtype: tuple
+    """
+    if b == 0:
+        return a, 1, 0
+    else:
+        d, x, y = extended_euclid(b, a % b)
+        return d, y, x - int(a / b) * y
+
+
 if __name__ == '__main__':
     print gcd(24, 30)
     print gcd(30, 24)
 
     print gcd_iterative(24, 30)
     print gcd_iterative(30, 24)
+    print extended_euclid(24, 30)
+    print extended_euclid(30, 24)
 
     print gcd_iterative(21, 30)
     print gcd(21, 30)
+
+    print extended_euclid(99, 78)
