187c16d362e122f7f2b1720e559fff27927c4be2
Suresh Sarda <sureshssarda@gmail.com>
2020-07-22 08:45:04 +0530

    Create ValidateStackSequence.java

---
+++ leetcode/ValidateStackSequence.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+/*
+Note this can be done in place as well
+*/
+class Solution {
+    public boolean validateStackSequences(int[] pushed, int[] popped) {
+        Deque<Integer> stack = new ArrayDeque<Integer>();
+        int pop = 0;
+        for (int push = 0; push < pushed.length; push++) {
+            stack.push(pushed[push]);
+            while (stack.size() > 0 && stack.peek() == popped[pop]) {
+                stack.pop();
+                pop++;
+            }
+        }
+        return stack.size() == 0;
+    }
+}
